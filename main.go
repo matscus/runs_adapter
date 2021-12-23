7c5e40f653b5a4bcea92cbefcab7d46e76f01f85
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2021-12-23 14:22:03 +0300

    migrate to gin and add handle

--- main.go
+++ main.go
@@ -11,100 +11,99 @@ import (
 	"os/signal"
 	"runs_adapter/adapter"
 	"runs_adapter/handlers"
-	"runs_adapter/middleware"
-	"strings"
+	"runtime"
 	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	pemPath, keyPath, proto, listenport, host, dbuser, dbpassword, dbhost, dbname, logLevel string
+	pemPath, keyPath, proto, host, listenport, logLevel, dbuser, dbpassword, dbhost, dbname string
 	dbport                                                                                  int
 	wait, writeTimeout, readTimeout, idleTimeout                                            time.Duration
+	debug, logger                                                                           bool
 )
 
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 func main() {
 	flag.StringVar(&pemPath, "pempath", os.Getenv("SERVERREM"), "path to pem file")
 	flag.StringVar(&keyPath, "keypath", os.Getenv("SERVERKEY"), "path to key file")
-	flag.StringVar(&listenport, "port", "10000", "port to Listen")
+	flag.StringVar(&listenport, "port", "9443", "port to Listen")
 	flag.StringVar(&proto, "proto", "http", "http or https")
+	flag.StringVar(&logLevel, "loglevel", "INFO", "log level, default INFO")
+	flag.BoolVar(&logger, "logger", false, "Gin logger Mode")
+	flag.BoolVar(&debug, "debug", false, "Gin debug Mode")
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully")
+	flag.DurationVar(&readTimeout, "read-timeout", time.Second*60, "read server timeout")
+	flag.DurationVar(&writeTimeout, "write-timeout", time.Second*60, "write server timeout")
+	flag.DurationVar(&idleTimeout, "idle-timeout", time.Second*60, "idle server timeout")
 	flag.StringVar(&dbuser, "user", "postgres", "db user")
 	flag.StringVar(&dbpassword, "password", `postgres`, "db user password")
-	flag.StringVar(&dbhost, "host", "localhost", "db host")
-	flag.StringVar(&logLevel, "loglevel", "INFO", "log level, default INFO")
+	flag.StringVar(&dbhost, "dbhost", "172.27.192.59", "db host")
 	flag.IntVar(&dbport, "dbport", 5432, "db port")
-	flag.StringVar(&dbname, "dbname", "runs", "db name")
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully")
-	flag.DurationVar(&readTimeout, "read-timeout", time.Second*15, "read server timeout")
-	flag.DurationVar(&writeTimeout, "write-timeout", time.Second*15, "write server timeout")
-	flag.DurationVar(&idleTimeout, "idle-timeout", time.Second*60, "idle server timeout")
+	flag.StringVar(&dbname, "dbname", "test", "db name")
 	flag.Parse()
-	log.Info("Parse flag completed")
-	setLogLevel(logLevel)
-	log.Info("Set log level completed")
-	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/runs/new", middleware.Middleware(handlers.NewHandler)).Methods(http.MethodPost, http.MethodOptions)
-	r.HandleFunc("/api/v1/runs/get/lasts", middleware.Middleware(handlers.LastsHandler)).Methods(http.MethodGet, http.MethodOptions).Queries("count", "{count}")
-	r.HandleFunc("/api/v1/runs/get/range", middleware.Middleware(handlers.RangeHandler)).Methods(http.MethodGet, http.MethodOptions).Queries("starttime", "{starttime}", "endtime", "{endtime}")
-	r.Handle("/metrics", promhttp.Handler())
-	r.HandleFunc("/debug/pprof/", pprof.Index)
-	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	r.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
-	r.Handle("/debug/pprof/block", pprof.Handler("block"))
-	r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	r.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
-	r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
-	http.Handle("/", r)
-	log.Info("Register handlers and route completed")
+	if !debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	var router *gin.Engine
+	if logger {
+		log.Info("Use gin Default")
+		router = gin.Default()
+	} else {
+		log.Info("Use gin new and recovery")
+		router = gin.New()
+		router.Use(gin.Recovery())
+	}
+	router.Use(handlers.Middleware())
+	ppof := router.Group("/pprof")
+	ppof.GET("/", gin.WrapF(pprof.Index))
+	ppof.GET("/cmdline", gin.WrapF(pprof.Cmdline))
+	ppof.GET("/profile", gin.WrapF(pprof.Profile))
+	ppof.POST("/symbol", gin.WrapF(pprof.Symbol))
+	ppof.GET("/symbol", gin.WrapF(pprof.Symbol))
+	ppof.GET("/trace", gin.WrapF(pprof.Trace))
+	ppof.GET("/allocs", gin.WrapH(pprof.Handler("allocs")))
+	ppof.GET("/block", gin.WrapH(pprof.Handler("block")))
+	ppof.GET("/goroutine", gin.WrapH(pprof.Handler("goroutine")))
+	ppof.GET("/heap", gin.WrapH(pprof.Handler("heap")))
+	ppof.GET("/mutex", gin.WrapH(pprof.Handler("mutex")))
+	ppof.GET("/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
+	router.GET("/metrics", handlers.PrometheusHandler())
+	v1 := router.Group("/api/v1")
+	{
+		v1.Any("/spaces", handlers.Spaces)
+		v1.Any("/projects", handlers.Projects)
+		v1.Any("/releases", handlers.Releases)
+		v1.Any("/versions", handlers.Versions)
+		v1.Any("/testtypes", handlers.TestTypes)
+		runs := v1.Group("/runs")
+		{
+			runs.Any("/", handlers.Runs)
+			runs.GET("/runid", handlers.LastRunID)
+			runs.GET("/space", handlers.SpaceRuns)
+			runs.GET("/project", handlers.ProjectRuns)
+			runs.GET("/release", handlers.ReleaseRuns)
+			runs.GET("/version", handlers.VersionRuns)
+			runs.GET("/testtype", handlers.TestTypeRuns)
+		}
+	}
 	go func() {
 		for {
 			err := adapter.InitDB(fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpassword, dbname))
 			if err != nil {
 				log.Error(err)
+			} else {
+				log.Info("Connected db complete")
+				break
 			}
 			time.Sleep(10 * time.Second)
 		}
 	}()
-	//time.Sleep(2 * time.Second)
-	// scenarios := []adapter.Scenario{adapter.Scenario{
-	// 	Name:     "UC01",
-	// 	TPS:      1000,
-	// 	SLA:      5,
-	// 	Duration: 120},
-	// }
-	// run := adapter.Run{
-	// 	ID:        3,
-	// 	StartTime: time.Now(),
-	// 	EndTime:   time.Now().Add(1 * time.Hour),
-	// 	Data: adapter.Data{
-	// 		Project:     "TestProject",
-	// 		Grafanalink: "http://master:3000/d/In_The_Bus/jmeter-test-overview?orgId=1&refresh=10s&from=now-1h&to=now",
-	// 		Description: "test description",
-	// 		Status:      "state ok",
-	// 		Scenarios:   scenarios,
-	// 	},
-	// }
-	//log.Println(run)
-	//err := run.Insert()
-	//log.Println(err)
-	// res, err := adapter.GetAllRuns()
-	// if err != nil {
-	// 	log.Error("Get interface adress error: ", err.Error())
-	// }
 
-	// log.Println(res)
-	// log.Println(res[0].Data.Scenarios[0].Name)
-	// template, err := adapter.GetTableHTML(res)
-	// if err != nil {
-	// 	log.Error("GetTemplate error: ", err.Error())
-	// }
-	// log.Println(template)
-	// client := confluence.Client{Client: http.DefaultClient, URL: "https://100.65.36.14/wiki/rest/api/content/42068579", AuthBase64: "UkVNYXRza3VzOlNiZXJNb2FuNDY5OCEh"}
-	// client.Push([]byte(template))
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		log.Error("Get interface adress error: ", err.Error())
@@ -117,15 +116,15 @@ func main() {
 			}
 		}
 	}
-	log.Info("Get IPv4 addr completed")
+
 	srv := &http.Server{
-		Addr:         host + ":" + listenport,
+		//Addr:         host + ":" + listenport,
+		Addr:         "0.0.0.0:" + listenport,
 		WriteTimeout: writeTimeout,
 		ReadTimeout:  readTimeout,
 		IdleTimeout:  idleTimeout,
-		Handler:      r,
+		Handler:      router,
 	}
-	log.Info("Set server params completed")
 	go func() {
 		switch proto {
 		case "https":
@@ -145,23 +144,9 @@ func main() {
 	<-c
 	ctx, cancel := context.WithTimeout(context.Background(), wait)
 	defer cancel()
-	srv.Shutdown(ctx)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
 	log.Info("server shutting down")
 	os.Exit(0)
-}
-
-func setLogLevel(level string) {
-	level = strings.ToUpper(level)
-	switch level {
-	case "INFO":
-		log.SetLevel(log.InfoLevel)
-	case "WARN":
-		log.SetLevel(log.WarnLevel)
-	case "ERROR":
-		log.SetLevel(log.ErrorLevel)
-	case "DEBUG":
-		log.SetLevel(log.DebugLevel)
-	case "TRACE":
-		log.SetLevel(log.TraceLevel)
-	}
 }
