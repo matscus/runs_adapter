32d82b1e3747e4287beeaa04a209bb8f9b85963f
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2022-01-12 10:41:35 +0300

    refactor item name

--- handlers/Errors.go
+++ handlers/Errors.go
@@ -8,8 +8,8 @@ import (
 
 func CheckSQLError(c *gin.Context, err error) {
 	if err == sql.ErrNoRows {
-		c.JSON(200, gin.H{"Status": "error", "Message": "No data found for the specified parameters"})
+		c.JSON(200, gin.H{"status": "error", "message": "No data found for the specified parameters"})
 		return
 	}
-	c.JSON(500, gin.H{"Status": "error", "Message": err.Error()})
+	c.JSON(500, gin.H{"status": "error", "message": err.Error()})
 }
