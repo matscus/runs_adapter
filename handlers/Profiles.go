7a2f90d65be772857432bb155a8b42583c8940b7
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2022-01-12 10:42:23 +0300

    add keys and refactor

--- handlers/Profiles.go
+++ handlers/Profiles.go
@@ -20,7 +20,7 @@ func Profiles(c *gin.Context) {
 				CheckSQLError(c, err)
 				return
 			}
-			c.JSON(200, res)
+			c.JSON(200, gin.H{"status": "ok", "data": res})
 			return
 		}
 		id := c.Query("id")
@@ -31,17 +31,61 @@ func Profiles(c *gin.Context) {
 				CheckSQLError(c, err)
 				return
 			}
-			c.JSON(200, res)
+			c.JSON(200, gin.H{"status": "ok", "data": res})
 			return
 		}
+		spaceName := c.Query("space")
+		if spaceName == "" {
+			c.JSON(400, gin.H{"status": "error", "Message": "param space is empty"})
+			return
+		}
+		projectName := c.Query("project")
+		if projectName == "" {
+			c.JSON(400, gin.H{"status": "error", "Message": "param project is empty"})
+			return
+		}
+		releaseName := c.Query("release")
+		if releaseName == "" {
+			c.JSON(400, gin.H{"status": "error", "Message": "param release is empty"})
+			return
+		}
+		versionName := c.Query("version")
+		if versionName == "" {
+			if releaseName == "" {
+				c.JSON(400, gin.H{"status": "error", "Message": "param version is empty"})
+				return
+			}
+		}
+		testTypeName := c.Query("testtype")
+		if testTypeName == "" {
+			if releaseName == "" {
+				c.JSON(400, gin.H{"status": "error", "Message": "param testtype is empty"})
+				return
+			}
+		}
+		res, err := adapter.GetProfiles(spaceName, projectName, releaseName, versionName, testTypeName)
+		if err != nil {
+			CheckSQLError(c, err)
+			return
+		}
+		c.JSON(200, gin.H{"status": "ok", "data": res})
+		return
+
 	case http.MethodPost:
 		profile := adapter.Profile{}
 		err := c.BindJSON(&profile)
 		if err != nil {
-			c.JSON(500, gin.H{"Status": "error", "Message": err.Error()})
+			c.JSON(500, gin.H{"status": "error", "Message": err.Error()})
 			return
 		}
-		profile.ID = uuid.New()
+		new := false
+		oldProfile, err := adapter.GetProfile(profile.SpaceName, profile.ProjectName, profile.ReleaseName, profile.VersionName, profile.TestTypeName, profile.Name)
+		if err == sql.ErrNoRows {
+			profile.ID = uuid.New()
+			new = true
+		} else {
+			profile.ID = oldProfile.ID
+		}
 		if profile.SpaceID == nilUUID {
 			profile.SpaceID, err = adapter.GetSpaceID(profile.SpaceName)
 			if err != nil {
@@ -122,18 +166,28 @@ func Profiles(c *gin.Context) {
 				}
 			}
 		}
-		_, err = profile.Create()
+		if new {
+			_, err = profile.Create()
+			if err != nil {
+				CheckSQLError(c, err)
+				return
+			}
+			c.JSON(200, gin.H{"status": "ok", "Message": "Profile created", "ID": profile.ID.String()})
+			return
+		}
+		_, err = profile.Update()
 		if err != nil {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Profile created", "ID": profile.ID.String()})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Profile updated"})
 		return
+
 	case http.MethodPut:
 		profile := adapter.Profile{}
 		err := c.BindJSON(&profile)
 		if err != nil {
-			c.JSON(500, gin.H{"Status": "error", "Message": err.Error()})
+			c.JSON(500, gin.H{"status": "error", "Message": err.Error()})
 			return
 		}
 
@@ -143,12 +197,12 @@ func Profiles(c *gin.Context) {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Profile updated", "ID": profile.ID.String()})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Profile updated", "ID": profile.ID.String()})
 		return
 	case http.MethodDelete:
 		id := c.Query("id")
 		if id == "" {
-			c.JSON(400, gin.H{"Status": "error", "Message": "param id is empty"})
+			c.JSON(400, gin.H{"status": "error", "Message": "param id is empty"})
 			return
 		}
 		uuid := uuid.MustParse(id)
@@ -157,6 +211,6 @@ func Profiles(c *gin.Context) {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Profile deleted"})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Profile deleted"})
 	}
 }
