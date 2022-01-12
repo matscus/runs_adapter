8a1787b655cf0969abf56d6dfc5e39288376a6ba
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2022-01-12 10:42:49 +0300

    fix

--- handlers/Releases.go
+++ handlers/Releases.go
@@ -18,7 +18,7 @@ func Releases(c *gin.Context) {
 				CheckSQLError(c, err)
 				return
 			}
-			c.JSON(200, res)
+			c.JSON(200, gin.H{"status": "ok", "data": res})
 			return
 		}
 		id := c.Query("id")
@@ -29,36 +29,41 @@ func Releases(c *gin.Context) {
 				CheckSQLError(c, err)
 				return
 			}
-			c.JSON(200, res)
+			c.JSON(200, gin.H{"status": "ok", "data": res})
 			return
 		}
 		spaceName := c.Query("space")
 		if spaceName == "" {
-			c.JSON(400, gin.H{"Status": "error", "Message": "param space is empty"})
+			c.JSON(400, gin.H{"status": "error", "Message": "param space is empty"})
 			return
 		}
 		projectName := c.Query("project")
 		if projectName == "" {
-			c.JSON(400, gin.H{"Status": "error", "Message": "param project is empty"})
+			c.JSON(400, gin.H{"status": "error", "Message": "param project is empty"})
 			return
 		}
 		releaseName := c.Query("release")
 		if projectName == "" {
-			c.JSON(400, gin.H{"Status": "error", "Message": "param release is empty"})
+			res, err := adapter.GetReleases(spaceName, projectName)
+			if err != nil {
+				CheckSQLError(c, err)
+				return
+			}
+			c.JSON(200, gin.H{"status": "ok", "data": res})
 			return
 		}
-		res, err := adapter.GetRelease(spaceName, releaseName, projectName)
+		res, err := adapter.GetRelease(spaceName, projectName, releaseName)
 		if err != nil {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, res)
+		c.JSON(200, gin.H{"status": "ok", "data": res})
 		return
 	case http.MethodPost:
 		release := adapter.Release{}
 		err := c.BindJSON(&release)
 		if err != nil {
-			c.JSON(500, gin.H{"Status": "error", "Message": err.Error()})
+			c.JSON(500, gin.H{"status": "error", "Message": err.Error()})
 			return
 		}
 		release.ID = uuid.New()
@@ -67,13 +72,13 @@ func Releases(c *gin.Context) {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Release created", "ID": release.ID.String()})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Release created", "ID": release.ID.String()})
 		return
 	case http.MethodPut:
 		release := adapter.Release{}
 		err := c.BindJSON(&release)
 		if err != nil {
-			c.JSON(500, gin.H{"Status": "error", "Message": err.Error()})
+			c.JSON(500, gin.H{"status": "error", "Message": err.Error()})
 			return
 		}
 		_, err = release.Update()
@@ -81,12 +86,12 @@ func Releases(c *gin.Context) {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Release updated"})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Release updated"})
 		return
 	case http.MethodDelete:
 		id := c.Query("id")
 		if id == "" {
-			c.JSON(400, gin.H{"Status": "error", "Message": "param id is empty"})
+			c.JSON(400, gin.H{"status": "error", "Message": "param id is empty"})
 			return
 		}
 		uuid := uuid.MustParse(id)
@@ -95,6 +100,6 @@ func Releases(c *gin.Context) {
 			CheckSQLError(c, err)
 			return
 		}
-		c.JSON(200, gin.H{"Status": "ok", "Message": "Release deleted"})
+		c.JSON(200, gin.H{"status": "ok", "Message": "Release deleted"})
 	}
 }
